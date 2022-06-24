ab65fca6c7b3badf68af714584583bb91710286c
Ankit kumar <akg95469@gmail.com>
2022-06-24 23:32:11 +0530

    My first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//When variable is declared but not assigned value its value is undefined but when the variavle itself is not declared but called in code then it is notdefined
+
+
+console.log(x);
+var x= 45;
+console.log(b);
+console.log(x);
